473cbd630b3871d8ce47808e8981252e6f6cd94a
JM <jalilmarkel1992@gmail.com>
2022-10-24 17:22:12 -0400

    url design finished

--- shifts/urls.py
+++ shifts/urls.py
@@ -8,4 +8,7 @@ router.register("", ShiftViewSet, "shifts")
 
 urlpatterns = [
     path("", include(router.urls)),
+    # path("<int:pk>/add-vehicle/", views.ShiftAddVehicleView.as_view(), name="shift_add_vehicle"),
+    # path("<int:pk>/check-status/", views.ShiftStatusView.as_view(), name="shift_check_status"),
+    # path("auto-create/", views.ShiftAutoCreateView.as_view(), name="shift_auto_create"),
 ]
